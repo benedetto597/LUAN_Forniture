ecbed90f85f20ea0210827fc92dcff8a3b91398b
Benedeπo <54255731+benedetto597@users.noreply.github.com>
2022-07-19 00:23:54 -0600

    Spring Boot Project

---
+++ src/main/java/io/benedetto/luanforniture/LuanfornitureApplication.java
@@ -0,0 +1,13 @@
+package io.benedetto.luanforniture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuanfornitureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuanfornitureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/benedetto/luanforniture/LuanfornitureApplicationTests.java
@@ -0,0 +1,13 @@
+package io.benedetto.luanforniture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuanfornitureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
